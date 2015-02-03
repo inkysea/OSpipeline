3578bc845477f730976603988eea7446a3a5ac9f
kris-thieler <kthieler@Kriss-MacBook-Pro.local>
2015-02-03 10:23:21 -0500

    Changed case
    Changed case on ISO name for consistency

--- CentOS66-x86_64/build.sh
+++ CentOS66-x86_64/build.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-mkisofs -r -T -J -V "CENTOS6X8664" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o bin/centOS6-x86_64.iso CentOS66-x86_64
+mkisofs -r -T -J -V "CENTOS6X8664" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o bin/CentOS6-x86_64.iso CentOS66-x86_64
 
 mkdir -p bin
 cp CentOS66-x86_64-ks.cfg bin/
