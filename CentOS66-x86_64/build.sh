c12eca5a2dce11f42f6cbde25149db2e6ca742ce
kris-thieler <kthieler@Kriss-MacBook-Pro.local>
2015-02-04 16:23:59 -0500

    KS file local
    Updated to keep the ks file local to the iso

--- CentOS66-x86_64/build.sh
+++ CentOS66-x86_64/build.sh
@@ -2,9 +2,12 @@
 
 cd `dirname $0`
 
+cp CentOS66-x86_64-ks.cfg CentOS66-x86_64/ks.cfg
+
 mkisofs -r -T -J -V "CENTOS6X8664" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o bin/CentOS6-x86_64.iso CentOS66-x86_64
+
+rm CentOS66-x86_64/ks.cfg
 
 mkdir -p bin
 cp CentOS66-x86_64-ks.cfg bin/
 cp ../post-installation/* bin/
-
