538e586fac3a3c3968552ccd54e5a1e213411be2
kris-thieler <kthieler@Kriss-MacBook-Pro.local>
2015-02-05 21:56:20 -0500

    Fixed gugent installation
    Fixed gouging installation to call gugent commands to advance machine
    lifecycle.

--- post-installation/install_gugent.sh
+++ post-installation/install_gugent.sh
@@ -1,19 +1,36 @@
 ##################### install_gugent.sh #######################
+
+# process funtion necessary for utilizing the gugent with kickstart
+function process() {
+    while true; do
+        /usr/bin/gugent --host=vra-iaas-2.inkysea.local --ssl --config=/usr/share/gugent/gugent.properties --script=/usr/share/gugent/site
+        if [ $? -eq 0 ]; then
+            break
+        fi
+        sleep 30
+    done
+}
+
 cd /tmp/
 wget http://devops.inkysea.com/pub/LinuxGuestAgentPkgs/rhel6-amd64/gugent-6.2.0-103014.x86_64.rpm
 rpm -i /tmp/gugent-6.2.0-103014.x86_64.rpm
+
 export AXIS2C_HOME=axis2
+export LD_LIBRARY_PATH=axis2/lib
 export PYTHONPATH=/usr/share/gugent/site/dops
+
 pushd /usr/share/gugent
-cd /etc/sysconfig
-mv selinux selinux.old
-#wget http://webserver/selinux
 cd /usr/share/gugent
 echo | openssl s_client -connect vra-iaas-2.inkysea.local:443 2>&1 | sed -ne "/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p" > cert.pem
 /usr/share/gugent/installgugent.sh vra-iaas-2.inkysea.local:443 ssl
-/usr/share/gugent/rungugent.sh &
+
+# Call process function twice to execute vRA workitems
 process # SetupOS
 process # CustomizeOS
+
+# run gugent and send to background so kickstart won't hang
+/bin/sh /usr/share/gugent/rungugent.sh > /dev/null 2>&1 < /dev/null &
+
 popd
 
 ##################### End install_gugent.sh ####################
