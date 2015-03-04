c3556fd669def23c34c25cae98c8bc806d8beb1f
kris-thieler <kthieler@Kriss-MacBook-Pro.local>
2015-03-04 17:10:53 -0500

    Updated for pipeline
    Updated ks and build scripts

--- CentOS66-x86_64/build.sh
+++ CentOS66-x86_64/build.sh
@@ -4,10 +4,17 @@ cd `dirname $0`
 
 cp CentOS66-x86_64-ks.cfg CentOS66-x86_64/ks.cfg
 
+mkdir -p bin
+rm -rf bin/*
+
 mkisofs -r -T -J -V "CENTOS6X8664" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o bin/CentOS6-x86_64.iso CentOS66-x86_64
 
-rm CentOS66-x86_64/ks.cfg
+#rm CentOS66-x86_64/ks.cfg
 
-mkdir -p bin
+
 cp CentOS66-x86_64-ks.cfg bin/
-cp ../post-installation/* bin/
+
+cd ..
+
+tar -cf CentOS66-x86_64/bin/scripts.tar post-installation/*.sh
+gzip CentOS66-x86_64/bin/scripts.tar
--- post-installation/install_gugent.sh
+++ post-installation/install_gugent.sh
@@ -12,8 +12,19 @@ function process() {
 }
 
 cd /tmp/
-wget http://devops.inkysea.com/pub/LinuxGuestAgentPkgs/rhel6-amd64/gugent-6.2.0-103014.x86_64.rpm
+
+
+wget -q http://vra-appserv-2.inkysea.com/agent/jre-1.7.0_72-lin64.zip
+mkdir /opt/vmware-jre
+unzip -d /opt/vmware-jre jre-1.7.0_72-lin64.zip
+
+wget -q http://vra-appserv-2.inkysea.com/agent/vmware-appdirector-agent-service-vcac_6.0.0.0-0_x86_64.rpm
+
+
+wget -q http://devops.inkysea.com/pub/LinuxGuestAgentPkgs/rhel6-amd64/gugent-6.2.0-103014.x86_64.rpm
 rpm -i /tmp/gugent-6.2.0-103014.x86_64.rpm
+
+rpm -i vmware-appdirector-agent-service-vcac_6.0.0.0-0_x86_64.rpm
 
 export AXIS2C_HOME=axis2
 export LD_LIBRARY_PATH=axis2/lib
@@ -22,11 +33,15 @@ export PYTHONPATH=/usr/share/gugent/site/dops
 pushd /usr/share/gugent
 cd /usr/share/gugent
 echo | openssl s_client -connect vra-iaas-2.inkysea.local:443 2>&1 | sed -ne "/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p" > cert.pem
+
 /usr/share/gugent/installgugent.sh vra-iaas-2.inkysea.local:443 ssl
+/opt/vmware-appdirector/agent-bootstrap/vcac-register.sh -r 443 -s vra-iaas-2.inkysea.local 
 
 # Call process function twice to execute vRA workitems
 process # SetupOS
 process # CustomizeOS
+
+
 
 # run gugent and send to background so kickstart won't hang
 /bin/sh /usr/share/gugent/rungugent.sh > /dev/null 2>&1 < /dev/null &
