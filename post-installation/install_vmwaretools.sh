fd588206c04fef6d805386c7233eef77b70d6302
kris-thieler <Kris.thieler@gmail.com>
2015-06-24 14:43:14 -0400

    Tokenized URLS

--- post-installation/install_vmwaretools.sh
+++ post-installation/install_vmwaretools.sh
@@ -1,5 +1,5 @@
 cd /tmp/
-wget http://devops.inkysea.com/pub/VMwareTools-9.4.5-1598834.tar.gz
+wget -q --no-check-certificate https://<--VRA_FQDN-->/artifactory/VMWareTools/VMwareTools-9.4.5-1598834.tar.gz
 tar -zxvf VMwareTools-9.4.5-1598834.tar.gz 
 cd vmware-tools-distrib
 ./vmware-install.pl -d
