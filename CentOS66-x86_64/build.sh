bc6cd1974ff1586f68d439adac4603ccffa728e0
kris-thieler <Kris.thieler@gmail.com>
2015-06-24 14:55:50 -0400

    Tokenized URLS

--- CentOS66-x86_64/build.sh
+++ CentOS66-x86_64/build.sh
@@ -2,6 +2,12 @@
 
 cd `dirname $0`
 
+PKG_LOCATION = $1
+PREP_SCRIPT_LOCATION = $2
+VRA_FQDN = $3
+VRA_APPS_FQDN = $4
+VRA_IAAS_FQDN = $5
+
 cp CentOS66-x86_64-ks.cfg CentOS66-x86_64/ks.cfg
 
 mkdir -p bin
@@ -9,12 +15,16 @@ rm -rf bin/*
 
 mkisofs -r -T -J -V "CENTOS6X8664" -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o bin/CentOS6-x86_64.iso CentOS66-x86_64
 
-#rm CentOS66-x86_64/ks.cfg
-
+sed -i 's/<--PKG_LOCATION-->/${PKG_LOCATION}/g' CentOS66-x86_64/ks.cfg
+sed -i 's/<---VRA_FQDN-->/${-VRA_FQDN}/g' CentOS66-x86_64/ks.cfg
 
 cp CentOS66-x86_64-ks.cfg bin/
 
 cd ..
+
+find post-installation/ -type f -exec sed -i 's/<--VRA_FQDN-->/${VRA_FQDN}/g' {} \;
+find post-installation/ -type f -exec sed -i 's/<--VRA_APPS_FQDN-->/${VRA_APPS_FQDN}/g' {} \;
+find post-installation/ -type f -exec sed -i 's/<--VRA_IAAS_FQDN-->/${VRA_IAAS_FQDN}/g' {} \;
 
 tar -cf CentOS66-x86_64/bin/scripts.tar post-installation/*.sh
 gzip CentOS66-x86_64/bin/scripts.tar
