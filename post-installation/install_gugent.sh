ed5118af33eeffa7fd4607ecd5922a051a485180
kris-thieler <Kris.thieler@gmail.com>
2015-06-24 14:39:41 -0400

    Tokenized URLS

--- post-installation/install_gugent.sh
+++ post-installation/install_gugent.sh
@@ -3,7 +3,7 @@
 # process funtion necessary for utilizing the gugent with kickstart
 function process() {
     while true; do
-        /usr/bin/gugent --host=vra-iaas-2.inkysea.local --ssl --config=/usr/share/gugent/gugent.properties --script=/usr/share/gugent/site
+        /usr/bin/gugent --host=<--VRA_FQDN--> --ssl --config=/usr/share/gugent/gugent.properties --script=/usr/share/gugent/site
         if [ $? -eq 0 ]; then
             break
         fi
@@ -14,15 +14,15 @@ function process() {
 cd /tmp/
 
 
-wget -q http://vra-appserv-2.inkysea.com/agent/jre-1.7.0_72-lin64.zip
+wget -q http://<--VRA_APPS_FQDN-->/agent/jre-1.7.0_72-lin64.zip
 mkdir /opt/vmware-jre
 unzip -d /opt/vmware-jre jre-1.7.0_72-lin64.zip
 
-wget -q http://vra-appserv-2.inkysea.com/agent/vmware-appdirector-agent-service-vcac_6.0.0.0-0_x86_64.rpm
+wget -q http://<--VRA_APPS_FQDN-->/agent/vmware-appdirector-agent-service-vcac_6.0.0.0-0_x86_64.rpm
 
+wget --no-check-certificate -q https://<--VRA_FQDN-->:5480/installer/LinuxGuestAgentPkgs.zip
 
-wget -q http://devops.inkysea.com/pub/LinuxGuestAgentPkgs/rhel6-amd64/gugent-6.2.0-103014.x86_64.rpm
-rpm -i /tmp/gugent-6.2.0-103014.x86_64.rpm
+rpm -i /tmp/LinuxGuestAgentPkgs/rhel6-amd64/gugent-6.2.0-103014.x86_64.rpm
 
 rpm -i vmware-appdirector-agent-service-vcac_6.0.0.0-0_x86_64.rpm
 
@@ -32,10 +32,10 @@ export PYTHONPATH=/usr/share/gugent/site/dops
 
 pushd /usr/share/gugent
 cd /usr/share/gugent
-echo | openssl s_client -connect vra-iaas-2.inkysea.local:443 2>&1 | sed -ne "/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p" > cert.pem
+echo | openssl s_client -connect <--VRA_IAAS_FQDN-->:443 2>&1 | sed -ne "/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p" > cert.pem
 
-/usr/share/gugent/installgugent.sh vra-iaas-2.inkysea.local:443 ssl
-/opt/vmware-appdirector/agent-bootstrap/vcac-register.sh -r 443 -s vra-iaas-2.inkysea.local 
+/usr/share/gugent/installgugent.sh <--VRA_IAAS_FQDN-->:443 ssl
+/opt/vmware-appdirector/agent-bootstrap/vcac-register.sh -r 443 -s <--VRA_IAAS_FQDN-->
 
 # Call process function twice to execute vRA workitems
 process # SetupOS
